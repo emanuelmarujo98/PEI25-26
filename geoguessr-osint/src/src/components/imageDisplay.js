0f6bfb785f76ebec54e36385ef30580a83f24967
emanuelmarujo98 <emanmarujo@gmal.com>
2026-06-13 12:14:03 +0100

    Versão final

--- geoguessr-osint/src/src/components/imageDisplay.js
+++
@@ -1,9 +0,0 @@
-export default function ImageDisplay({ image }) {
-  return (
-    <img
-      src={image}
-      alt="Local"
-      style={{ width: "200%", maxHeight: "500px", objectFit: "contain" }}
-    />
-  );
-}
